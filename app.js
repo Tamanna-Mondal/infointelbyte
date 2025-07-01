d087425c61fa01af15d6a8832e18c86575c9f751
Tamanna Mondal <info@intelbyte.in>
2025-07-01 16:14:45 +0530

    Deploy to Azure App Service

--- app.js
+++ app.js
@@ -5,20 +5,10 @@ const PORT = 8080;
 
 
 
-//DataBase Part
-// const mongoose = require('mongoose');
-// const DBURL = process.env.ATLASDB_URL;
-// MongoDB Connection
-// mongoose.connect(DBURL)
-//     .then(() => console.log('DB Connected...'))
-//     .catch(err => console.log(err));
-
 
     
 const methodOverride = require('method-override') ;
-// const session = require('express-session');
-// const passport = require('passport');
-// const passportLocal = require('passport-local');
+
 const flash = require('connect-flash');
 const path = require('path');
 const ejsMate = require('ejs-mate');
@@ -35,24 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 
-// Session Configuration
-
-
-// Passport Authentication
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new passportLocal(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-
-// Flash Messages
-// app.use(flash());
-// app.use((req, res, next) => {
-//     res.locals.success = req.flash('success');
-//     res.locals.error = req.flash('error');
-//     res.locals.currUser = req.user || null;
-//     next();
-// });
 
 
 // Routing 
@@ -75,50 +47,8 @@ app.get('/intelbyte/:page', (req,res) =>{
 })
 
 
-app.post('/quote', async (req, res) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    mobile,
-    service,
-    startTime,
-    budget,
-    projectDetails
-  } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: 'info@intelbyte.in', // or 'Outlook', 'Yahoo', etc.
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
-    }
-  });
 
-  const mailOptions = {
-    from: email,
-    to: process.env.COMPANY_EMAIL,
-    subject: `New Demo Request from ${firstName} ${lastName}`,
-    text: `
-      Name: ${firstName} ${lastName}
-      Email: ${email}
-      Mobile: ${mobile}
-      Service: ${service}
-      Start Time: ${startTime}
-      Budget: ${budget}
-      Project Details:
-      ${projectDetails}
-    `
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    res.send("Demo request sent successfully!");
-  } catch (err) {
-    console.error(err);
-    res.send("Failed to send demo request.");
-  }
-});
 
 
 app.listen(PORT, () => {
